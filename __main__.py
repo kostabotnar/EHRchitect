55f3a442e404ea8dea75156fe024b1bd41eb791a
Konstantin Botnar <kosta.botnar@gmail.com>
2023-12-29 12:03:57 -0600

    Add load data and run study API

--- __main__.py
+++ __main__.py
@@ -11,7 +11,6 @@ def runner(**kwargs):
         print(e)
 
 
-
 @click.group(context_settings=CONTEXT_SETTINGS)
 @click.version_option(version='1.0.0')
 def run():
@@ -41,6 +40,38 @@ def run():
                    'It will speed up data upload but increase data search time. Default value is TRUE')
 def createdb(**kwargs):
     runner(createdb=True, **kwargs)
+
+
+@run.command()
+@click.argument(arg.db_name)
+@click.option(f'--{opt.url}',
+              default=None,
+              help='URL obtained from the TriNetX export option. '
+                   'If it is null, --archive option is used as a data source. '
+                   'If neither --url nor --archive are set, an empty database will be created.')
+@click.option(f'--{opt.archive}',
+              default=None,
+              help='Archive file name to store data from URL. '
+                   'If URL is null, then this option is a path to the existing data archive. '
+                   'If it is null, it will be constructed from the database name and timestamp. '
+                   'If neither --url nor --archive are set, an empty database will be created.')
+@click.option(f'--{opt.local_access}',
+              default=True,
+              help='If False, then SSH connection will be used. '
+                   'Otherwise, the local host DB connection will be establish')
+@click.option(f'--{opt.set_index}',
+              default=True,
+              help='If FALSE, than table indexes will not be created. '
+                   'It will speed up data upload but increase data search time. Default value is TRUE')
+def load(**kwargs):
+    runner(load=True, **kwargs)
+
+
+@run.command()
+@click.argument(arg.db_name)
+@click.argument(arg.study, nargs=-1)
+def run_study(**kwargs):
+    runner(run_study=True, **kwargs)
 
 
 if __name__ == '__main__':
--- src/api.py
+++ src/api.py
@@ -1,9 +1,12 @@
 class Command:
     create_new_db = 'createdb'
+    load_data = 'load'
+    run_study = "run_study"
 
 
 class Argument:
     db_name = 'db_name'
+    study = 'study'
 
 
 class Option:
@@ -15,7 +18,11 @@ class Option:
 
 def validate(**kwargs) -> bool:
     if Command.create_new_db in kwargs:
-        validate_create_db(**kwargs)
+        return validate_create_db(**kwargs)
+    if Command.load_data in kwargs:
+        return validate_create_db(**kwargs)
+    if Command.run_study in kwargs:
+        return validate_run_study(**kwargs)
     return False
 
 
@@ -31,5 +38,21 @@ def validate_create_db(**kwargs) -> bool:
     if kwargs[Option.archive] is not None and kwargs[Option.archive].strip() == "":
         raise ValueError(
             f'Value Error: Invalid --{Option.archive} value: "{kwargs[Option.archive]}"'
+        )
+    return True
+
+
+def validate_run_study(**kwargs) -> bool:
+    if kwargs[Argument.db_name].strip() == "":
+        raise ValueError(
+            f'Value Error: Invalid {Argument.db_name} value: "{kwargs[Argument.db_name]}"'
+        )
+    if len(kwargs[Argument.study]) == 0:
+        raise ValueError(
+            f'Value Error: AT least one study should be set'
+        )
+    if any([s[-4:] != 'json' for s in kwargs[Argument.study]]):
+        raise ValueError(
+            f'Value Error: All study files should have JSON format'
         )
     return True
